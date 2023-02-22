e655b3ae7df99aee6fb6aedfaf7632767b0ca3ea
vedansh kumar <56169665+vedanshk@users.noreply.github.com>
2023-02-22 21:26:54 +0530

    finished app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css';
 import 'tachyons';
+
 const el = document.getElementById('root');
 
 const root = ReactDOM.createRoot(el);
